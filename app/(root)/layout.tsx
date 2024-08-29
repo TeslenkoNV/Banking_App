72d8415841516017d8f48745d7847d844df37739
TeslenkoNV <natella911@mail.ru>
2024-08-29 21:20:48 +0300

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,27 @@
-import { Sidebar } from "lucide-react";
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
 
 export default function Home({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const loggedIn = { firstName: 'Nataliya', lastName: 'JSM' };
+    
     return (
-      <main>
-          SIDEBAR
-              {children}
+      <main className="flex h-screen w-full font-inter">
+          <Sidebar user={loggedIn} />
+
+          <div className="flex size-full flex-col">
+            <div className="root-layout">
+              <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+              <div>
+                <MobileNav user={loggedIn} />
+              </div>
+            </div>
+            {children}
+          </div>
       </main>
     );
   }
